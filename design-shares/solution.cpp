df6895113a5079170b2e5ed6c1ab8de17208a05a
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-10-12 21:51:44 +0300

    design shares

---
+++ design-shares/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ali                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+
+int main()
+{
+    ali;
+    int n;
+    cin >> n;
+
+    cout << n - 1 << endl;
+}
